75d2c3dcfa79bdb861518e71febb19faf6a17328
FMCalisto <francisco_mcalisto@hotmail.com>
2016-03-08 19:00:11 +0000

    [UPDATE] Fixed Code

--- ttt/ttt-rmi-client/src/main/java/ttt/TTTClient.java
+++ ttt/ttt-rmi-client/src/main/java/ttt/TTTClient.java
@@ -1,86 +1,62 @@
 package ttt;
 
-import java.rmi.*;
+import java.rmi.RemoteException;
 import java.util.Scanner;
 
 public class TTTClient {
-	
-//	int winner = -1;
-//	static int player = 0;
-	
-	static Scanner keyboardSc;
-	
-	public static void main(String[] args) throws Exception {
-		int play;
-		int cont = 0;
-		int winner = -1;
-		int player = 0;
-		boolean acceptedPlay;
-		boolean restart;
-		
-		try {
-			System.out.println("Port search...");
-			TTTService game = (TTTService) Naming.lookup("//localhost:8080/TTT");
-			System.out.println("Port connection accepted");
-			
-			do{
-		 		restart=false;
-				do {
-					player = ++player % 2;
-					do {
-						System.out.println(game.currentBoard());
-						//play = readPlay();					
-						do {
-							System.out.printf("\nPlayer %d, please enter the number of the square "
-											+ "where you want to place your %c (or 0 to refresh the board): \n",
-											player, (player == 1) ? 'X' : 'O');
-							play = keyboardSc.nextInt();
-						} while (play > 10 || play < 0);
-						//
-						if (play != 0) {
-							if(play==10){
-								int last=game.lastPlay(player);
-								if(last!=-1){
-									System.out.println("Last play was: " + last +" by player " + player);
-								}else{
-									System.out.println("No plays made by player " + player);
-								}
-								acceptedPlay = false;
-								continue;
-							}
-							acceptedPlay = game.play( --play / 3, play % 3, player);
-							if (!acceptedPlay)
-								System.out.println("Invalid play! Try again.");
-						} else
-							acceptedPlay = false;
-					} while (!acceptedPlay);
-					winner = game.checkWinner();
-				} while (winner == -1);
-	
-				if (winner == 2)
-					System.out.printf("\nHow boring, it is a draw\n");
-				else
-					System.out.printf(
-							"\nCongratulations, player %d, YOU ARE THE WINNER!\n",
-							winner);
-				do{
-					System.out.println("Restart the game? (yes = 1/no = 2): ");
-					cont = keyboardSc.nextInt();
-				}while(cont < 1 || cont> 2);
-				
-				if(cont == 1){
-	  				game.restart();
-	  				restart=true;
-	  				play=0;
-	  				player = 1;
-	  				cont= 0;
-	  				}
-	
-	 		}while(restart);
-	 		
-			}catch(RemoteException e){
-				System.out.println("TTT:"+ e.getMessage());
-			}
-		}
+	TTT ttt;
+	Scanner keyboardSc;
+	int winner = 0;
+	int player = 1;
 
+	public TTTClient() throws RemoteException {
+		ttt = new TTT();
+		keyboardSc = new Scanner(System.in);
+	}
+
+	public int readPlay() {
+		int play;
+		do {
+			System.out.printf("\nPlayer %d, please enter the number of the square "
+							+ "where you want to place your %c (or 0 to refresh the board): \n",
+							player, (player == 1) ? 'X' : 'O');
+			play = keyboardSc.nextInt();
+		} while (play > 9 || play < 0);
+		return play;
+	}
+
+	public void playGame() {
+		int play;
+		boolean playAccepted;
+
+		do {
+			player = ++player % 2;
+			do {
+				System.out.println(ttt.currentBoard());
+				play = readPlay();
+				if (play != 0) {
+					playAccepted = ttt.play( --play / 3, play % 3, player);
+					if (!playAccepted)
+						System.out.println("Invalid play! Try again.");
+				} else
+					playAccepted = false;
+			} while (!playAccepted);
+			winner = ttt.checkWinner();
+		} while (winner == -1);
+	}
+
+	public void congratulate() {
+		if (winner == 2)
+			System.out.printf("\nHow boring, it is a draw\n");
+		else
+			System.out.printf(
+					"\nCongratulations, player %d, YOU ARE THE WINNER!\n",
+					winner);
+	}
+
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.playGame();
+		g.congratulate();
+	}
 }
