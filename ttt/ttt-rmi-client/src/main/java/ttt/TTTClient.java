dbe4ee336cfc07ee3e8529ac5925f1b9da21eebd
FMCalisto <francisco_mcalisto@hotmail.com>
2016-03-08 18:36:13 +0000

    [FIXME] Code Error

---
+++ ttt/ttt-rmi-client/src/main/java/ttt/TTTClient.java
@@ -0,0 +1,24 @@
+package ttt;
+
+import java.rmi.*;
+import java.util.Scanner;
+
+public class TTTClient {
+	
+	int winner = -1;
+	int player = 0;
+	
+	static Scanner keyboardSc;
+	
+	public static void main(String[] args) throws Exception {
+		int play;
+		int cont = 0;
+		boolean acceptedPlay;
+		boolean restart;
+		
+		try {
+			TTTService game = Naming.lookup("//localhost:/TTT");
+		}
+	}
+
+}
--- ttt/ttt-rmi-server/src/main/java/ttt/TTTServer.java
+++ ttt/ttt-rmi-server/src/main/java/ttt/TTTServer.java
@@ -1,11 +1,17 @@
 package ttt;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class TTTServer {
-	public static void main(String args[]) {
-		int registryPort = 8080;
+	public static void main(String args[]) throws IOException {
+		
+		@SuppressWarnings("resource")
+		ServerSocket s = new ServerSocket(0);		
+		int registryPort = s.getLocalPort();
+		System.out.println("listening on port: " + s.getLocalPort());
 		
 		System.out.println("Main OK");
 		
@@ -28,7 +34,7 @@ public class TTTServer {
 			System.out.println("TTT server main " + e.getMessage());
 		}
 		
-		System.out.println("TTT server EXIT");
+		System.out.println("Please start the client...");
 		
 	}
 
