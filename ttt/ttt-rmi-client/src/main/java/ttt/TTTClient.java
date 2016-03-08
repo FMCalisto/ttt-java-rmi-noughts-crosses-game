8206ebf877b686299c5f1ec8f8da6a95d876db22
FMCalisto <francisco_mcalisto@hotmail.com>
2016-03-08 18:53:08 +0000

    [FIXME] Code Error

--- ttt/ttt-rmi-client/src/main/java/ttt/TTTClient.java
+++ ttt/ttt-rmi-client/src/main/java/ttt/TTTClient.java
@@ -22,57 +22,59 @@ public class TTTClient {
 			System.out.println("Port search...");
 			TTTService game = (TTTService) Naming.lookup("//localhost:8080/TTT");
 			System.out.println("Port connection accepted");
-
-			public Game() {
-				ttt = new TTT();
-				keyboardSc = new Scanner(System.in);
-			}
-
-			public int readPlay() {
-				int play;
-				do {
-					System.out.printf("\nPlayer %d, please enter the number of the square "
-									+ "where you want to place your %c (or 0 to refresh the board): \n",
-									player, (player == 1) ? 'X' : 'O');
-					play = keyboardSc.nextInt();
-				} while (play > 9 || play < 0);
-				return play;
-			}
-
-			public void playGame() {
-				int play;
-				boolean playAccepted;
-
+			
+			do{
+		 		restart=false;
 				do {
 					player = ++player % 2;
 					do {
-						System.out.println(ttt.currentBoard());
-						play = readPlay();
+						System.out.println(game.currentBoard());
+						//play = readPlay();					
+						do {
+							System.out.printf("\nPlayer %d, please enter the number of the square "
+											+ "where you want to place your %c (or 0 to refresh the board): \n",
+											player, (player == 1) ? 'X' : 'O');
+							play = keyboardSc.nextInt();
+						} while (play > 10 || play < 0);
+						//
 						if (play != 0) {
-							playAccepted = ttt.play( --play / 3, play % 3, player);
-							if (!playAccepted)
+							if(play==10){
+								int last=game.lastPlay(player);
+								if(last!=-1){
+									System.out.println("Last play was: " + last +" by player " + player);
+								}else{
+									System.out.println("No plays made by player " + player);
+								}
+								acceptedPlay = false;
+								continue;
+							}
+							acceptedPlay = game.play( --play / 3, play % 3, player);
+							if (!acceptedPlay)
 								System.out.println("Invalid play! Try again.");
 						} else
-							playAccepted = false;
-					} while (!playAccepted);
-					winner = ttt.checkWinner();
+							acceptedPlay = false;
+					} while (!acceptedPlay);
+					winner = game.checkWinner();
 				} while (winner == -1);
-			}
-
-			public void congratulate() {
+	
 				if (winner == 2)
 					System.out.printf("\nHow boring, it is a draw\n");
 				else
 					System.out.printf(
 							"\nCongratulations, player %d, YOU ARE THE WINNER!\n",
 							winner);
-			}
-
-			public static void main(String[] args) {
-				Game g = new Game();
-				g.playGame();
-				g.congratulate();
-			}
+				do{
+					System.out.println("Restart the game? (yes = 1/no = 2): ");
+					cont = keyboardSc.nextInt();
+				}while(cont < 1 || cont> 2);
+				
+				if(cont == 1){
+	  				game.restart();
+	  				restart=true;
+	  				play=0;
+	  				player = 1;
+	  				cont= 0;
+	  				}
 	
 	 		}while(restart);
 	 		
